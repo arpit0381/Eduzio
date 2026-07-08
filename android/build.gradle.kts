1d3cc0904ddaaddb5530b8c0fa98764dcbecc92f
Arpit <arpitbajpai038@gmail.com>
2026-07-08 23:06:56 +0530

    hehhe

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -15,29 +15,27 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
+subprojects {
+    afterEvaluate {
+        val extension = extensions.findByName("android") as? com.android.build.gradle.BaseExtension
+        if (extension != null) {
+            // Force compileSdkVersion to 36 to resolve connectivity_plus and newer AndroidX library dependency errors
+            extension.compileSdkVersion(36)
+
+            // Set namespace if it is missing
+            if (extension.namespace == null) {
+                val groupPath = project.group.toString()
+                extension.namespace = if (groupPath.isNotEmpty()) groupPath else "com.eduzio.${project.name.replace("-", "_")}"
+            }
+        }
+    }
+}
+
 subprojects {
     project.evaluationDependsOn(":app")
 }
 
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
-}
-
-subprojects {
-    val proj = this
-    val configureProject = {
-        val extension = proj.extensions.findByName("android") as? com.android.build.gradle.BaseExtension
-        if (extension != null && extension.namespace == null) {
-            val groupPath = proj.group.toString()
-            extension.namespace = if (groupPath.isNotEmpty()) groupPath else "com.eduzio.${proj.name.replace("-", "_")}"
-        }
-    }
-
-    if (proj.state.executed) {
-        configureProject()
-    } else {
-        proj.afterEvaluate {
-            configureProject()
-        }
-    }
 }
